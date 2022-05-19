cb0d2d393bf328d9f44e72503501a04e23c34c13
Alper Özgün <103696767+alperozgun@users.noreply.github.com>
2022-05-20 02:38:13 +0300

    Create Hw4_.sql

---
+++ Hw4_.sql
@@ -0,0 +1,17 @@
+Senaryo 1)film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+SELECT DISTINCT replacement_cost FROM film
+
+Senaryo 2) film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+SELECT COUNT (DISTINCT replacement_cosT) FROM film
+
+Senaryo 3) film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
+SELECT COUNT (*) FROM film
+WHERE title LIKE 'T%' AND rating= 'G'
+
+Senaryo 4) country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+ELECT COUNT (*) FROM country 
+WHERE country LIKE '_____'
+
+Senaryo 5) city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+SELECT COUNT (*) FROM city
+WHERE city ILIKE '%R'
